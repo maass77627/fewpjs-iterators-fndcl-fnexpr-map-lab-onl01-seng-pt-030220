46d0b440868714ba748c16df02ca6ef33adf0478
kelly r maass <kmaass77627@gmail.com>
2020-10-04 22:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  tutorials.map(tutorial) {
-    tutorial.titleCase;
-  }
-}
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+   word => word[0].toUpperCase() + word.slice(1)
+).join(' ')
+
+)
